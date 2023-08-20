784894eca5803d12d72b9a8409b20d731e250c20
Bente Zuijdam <bzui6514@uni.sydney.edu.au>
2023-08-20 13:35:55 +1000

    test 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        System.out.println("Hello everyone!");
+        System.out.print("I have to test this");
     }
 }
